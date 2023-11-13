ebf54bc8609c309afbda06207965fb3f83d9ecd7
Michal Resnick <michalf3160@gmail.com>
2023-11-14 01:05:47 +0200

    service

--- Frontend/src/Services/DataService.ts
+++ Frontend/src/Services/DataService.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import TaskModel from "../Models/Task-Model";
 import appConfig from "../Utils/Config";
+import CustomerModel from "../Models/Customers-Model";
+import EmployeeModel from "../Models/Employee-Model";
 
 class DataService {
 
@@ -9,8 +11,58 @@ class DataService {
     const response=await axios.get<TaskModel[]>(appConfig.TasksUrl)
     const tasks=response.data
     return tasks
-
    }
+
+   public async getAllTasksByEmployeeName(id:number):Promise<TaskModel[]>{
+    const response=await axios.get<TaskModel[]>(appConfig.TasksByEmployeeUrl+id)
+    const tasksByEmployeeName=response.data
+    return tasksByEmployeeName
+   }
+
+   public async getAllTasksByCustomerName(id:number):Promise<TaskModel[]>{
+    const response=await axios.get<TaskModel[]>(appConfig.TasksByCustomerUrl+id)
+    const tasksByCustomerName=response.data
+    return tasksByCustomerName
+   }
+
+       
+   public async addTask(task:TaskModel):Promise<void>{
+    await axios.post<TaskModel>(appConfig.TasksUrl,task)
+ }
+
+public async updatetask(task:TaskModel):Promise<void>{
+    await axios.put<TaskModel>(appConfig.TasksUrl,task)
+ }
+
+ public async deletetask(taskId:number):Promise<void>{
+    await axios.delete<TaskModel>(appConfig.TasksUrl+taskId)
+ }
+
+ public async addCustomer(customer:CustomerModel):Promise<void>{
+    await axios.post<CustomerModel>(appConfig.CustomersUrl,customer)
+ }
+
+public async updatecustomer(customer:CustomerModel):Promise<void>{
+    await axios.put<CustomerModel>(appConfig.CustomersUrl,customer)
+ }
+
+ public async deletecustomer(customerId:number):Promise<void>{
+    await axios.delete<CustomerModel>(appConfig.CustomersUrl+customerId)
+ }
+
+ public async addEmployee(employee:EmployeeModel):Promise<void>{
+    await axios.post<EmployeeModel>(appConfig.EmployeesUrl,employee)
+ }
+
+public async updateEmployee(employee:EmployeeModel):Promise<void>{
+    await axios.put<EmployeeModel>(appConfig.EmployeesUrl,employee)
+ }
+
+ public async deleteEmployee(employeeId:number):Promise<void>{
+    await axios.delete<EmployeeModel>(appConfig.EmployeesUrl+employeeId)
+ }
+
+
 
 
 }
